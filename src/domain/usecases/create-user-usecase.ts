26b41ba5d264b346ea6d7aba94c7e8f7f0085e68
João Victor <jvg87.dev@gmail.com>
2024-12-31 18:34:22 -0300

    feat: ensure CreateUserUseCase calls hahs with correct password

--- src/domain/usecases/create-user-usecase.ts
+++ src/domain/usecases/create-user-usecase.ts
@@ -1,13 +1,20 @@
 import { User, UserParams } from "@/domain/entities";
-import { ICreateUser, IUserRepository } from "@/domain/protocols";
+import { ICreateUser, IHasher, IUserRepository } from "@/domain/protocols";
 
 export class CreateUserUseCase implements ICreateUser {
-  constructor(private readonly userRepository: IUserRepository) {}
+  constructor(
+    private readonly userRepository: IUserRepository,
+    private readonly hasher: IHasher
+  ) {}
   async execute(userParams: UserParams): Promise<User | null> {
     const { email, password } = userParams;
+
     const emailExists = await this.userRepository.checkByEmail(email);
 
     if (emailExists) return null;
+
+    const salt = 12;
+    await this.hasher.hash(password, salt);
 
     return {
       id: "string",
