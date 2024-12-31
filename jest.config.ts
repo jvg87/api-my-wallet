d8384c9c44ac9b5851f3fe5e6df3edb837aa71d6
João Victor <jvg87.dev@gmail.com>
2024-12-30 22:11:51 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+
+const config: Config = {
+  roots: ["<rootDir>/tests"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/main/**",
+    "!<rootDir>/src/**/index.ts",
+  ],
+  coverageDirectory: "coverage",
+  coverageProvider: "babel",
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+    "@/tests/(.*)": "<rootDir>/tests/$1",
+  },
+  clearMocks: true,
+};
+
+export default config;
