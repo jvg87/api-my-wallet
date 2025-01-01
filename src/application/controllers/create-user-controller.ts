36aa1c840ea10dcebaaa7e5500597a67817309bf
João Victor <jvg87.dev@gmail.com>
2024-12-31 23:11:43 -0300

    feat: ensure CreateUserController calls CreateUSer with correct values

--- src/application/controllers/create-user-controller.ts
+++ src/application/controllers/create-user-controller.ts
@@ -3,15 +3,20 @@ import {
   IHttpRequest,
   IHttpResponse,
 } from "@/application/protocols";
+import { ICreateUser } from "@/domain/protocols";
 import { MissingParamsError } from "../erros";
 import { badRequest } from "../helpers";
 
 export class CreateUserController implements IController {
+  constructor(private readonly createUserUseCase: ICreateUser) {}
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
     const { name, email, password } = request.body;
 
     const missingParam = this.validateParams({ name, email, password });
+
     if (missingParam) return badRequest(new MissingParamsError(missingParam));
+
+    await this.createUserUseCase.execute({ email, name, password });
 
     return {
       statusCode: 1,
