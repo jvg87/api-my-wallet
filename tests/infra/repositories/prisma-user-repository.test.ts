6e3a49baa13ed6704930f6aad00b75eb1761162a
João Victor <jvg87.dev@gmail.com>
2025-01-01 13:00:16 -0300

    test: ensure PrismaUserRepository.checkByEmail return true if email already exists

---
+++ tests/infra/repositories/prisma-user-repository.test.ts
@@ -0,0 +1,40 @@
+import { PrismaUserRepository } from "@/infra/repositories";
+import { prisma } from "@/utils";
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+} from "@jest/globals";
+
+describe("PrismaUser Repository", () => {
+  let sut: PrismaUserRepository;
+
+  beforeAll(() => {});
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  beforeEach(async () => {
+    sut = new PrismaUserRepository();
+    await prisma.user.deleteMany();
+  });
+
+  describe("checkByEmail()", () => {
+    it("Should return true if email exists", async () => {
+      await prisma.user.create({
+        data: {
+          email: "any_email@mail.com",
+          name: "any_name",
+          password: "any_password",
+        },
+      });
+
+      const isExists = await sut.checkByEmail("any_email@mail.com");
+      expect(isExists).toBeTruthy();
+    });
+  });
+});
