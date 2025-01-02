7874865b4e47f8a95dc06daf0934c162990935ae
João Victor <jvg87.dev@gmail.com>
2025-01-01 22:45:25 -0300

    test: ensure PrismaUserRepository.loadByEmail returns correctly

--- tests/infra/repositories/prisma-user-repository.test.ts
+++ tests/infra/repositories/prisma-user-repository.test.ts
@@ -56,4 +56,26 @@ describe("PrismaUser Repository", () => {
       expect(user?.name).toBe("any_name");
     });
   });
+
+  describe("findByEmail()", () => {
+    it("Should return a user if success", async () => {
+      const newUser = await prisma.user.create({
+        data: {
+          email: "any_email@mail.com",
+          name: "any_name",
+          password: "any_password",
+        },
+      });
+
+      const user = await sut.findByEmail(newUser.email);
+      expect(user).toBeTruthy();
+      expect(user?.id).toBe(newUser.id);
+      expect(user?.email).toBe(newUser.email);
+    });
+
+    it("Should return null if user not found", async () => {
+      const user = await sut.findByEmail("any_email@mail.com");
+      expect(user).toBeNull();
+    });
+  });
 });
