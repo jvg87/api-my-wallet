39d2d5a14552a1036bc356e64710c339dd37426c
João Victor <jvg87.dev@gmail.com>
2025-01-03 21:40:46 -0300

    feat: ensure BcryptAdapter calls comapre method with correct values

--- src/infra/adapters/bcrypt-adapter.ts
+++ src/infra/adapters/bcrypt-adapter.ts
@@ -1,9 +1,14 @@
-import { IHasher } from "@/domain/protocols";
+import { IHashComparer, IHasher } from "@/domain/protocols";
 
 import bcrypt from "bcrypt";
 
-export class BcryptAdapter implements IHasher {
+export class BcryptAdapter implements IHasher, IHashComparer {
   async hash(text: string, salt: number): Promise<string> {
     return bcrypt.hash(text, salt);
+  }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(value, hash);
+    return isValid;
   }
 }
