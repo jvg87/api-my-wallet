5eb46951e8c96f46523136ff1f7e880d1897ee0d
João Victor <jvg87.dev@gmail.com>
2025-01-03 22:35:36 -0300

    feat: ensure AuthUserController calls AuthUser with correct values

--- src/application/controllers/auth-user-controller.ts
+++ src/application/controllers/auth-user-controller.ts
@@ -5,14 +5,18 @@ import {
   IHttpRequest,
   IHttpResponse,
 } from "@/application/protocols";
+import { IAuthUser } from "@/domain/protocols";
 
 export class AuthUserController implements IController {
+  constructor(private readonly authUser: IAuthUser) {}
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
     const { email, password } = request.body;
 
     if (!email) return badRequest(new MissingParamsError("email"));
 
     if (!password) return badRequest(new MissingParamsError("password"));
+
+    await this.authUser.execute({ email, password });
 
     return {
       statusCode: 1,
