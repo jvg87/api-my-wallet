6992a74e99cee944c962cc57a3e1c27fe368c433
João Victor <jvg87.dev@gmail.com>
2025-01-03 22:27:32 -0300

    test: ensure AuthUserController retrns 400 fi no email is provided

---
+++ tests/application/controllers/auth-user-controller.spec.ts
@@ -0,0 +1,39 @@
+import {
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  jest,
+} from "@jest/globals";
+
+import { AuthUserController } from "@/application/controllers";
+import { MissingParamsError } from "@/application/erros";
+import { badRequest } from "@/application/helpers";
+import { IHttpRequest } from "@/application/protocols";
+import { IAuthUser } from "@/domain/protocols";
+import { mockAuthUser } from "@/tests/domain/mocks";
+
+describe("AuthUser Controller", () => {
+  const mockAuthUserUseCase: jest.Mocked<IAuthUser> = {
+    execute: jest.fn(),
+  };
+
+  let sut: AuthUserController;
+
+  beforeAll(() => {
+    mockAuthUserUseCase.execute.mockResolvedValue(mockAuthUser());
+  });
+
+  beforeEach(() => {
+    sut = new AuthUserController();
+  });
+
+  it("Should return 400 if no email is provided ", async () => {
+    const request: IHttpRequest = {
+      body: {},
+    };
+    const httpResponse = await sut.handle(request);
+    expect(httpResponse).toEqual(badRequest(new MissingParamsError("email")));
+  });
+});
