efd49f5186e09b102b1d6ba1d427fcc209481448
João Victor <jvg87.dev@gmail.com>
2025-01-11 17:43:10 -0300

    feat: ensure AuthMiddleware returns 500 if decrypter throws

--- src/application/middlewares/auth-middleware.ts
+++ src/application/middlewares/auth-middleware.ts
@@ -1,5 +1,5 @@
 import { UnauthorizedError } from "@/application/erros";
-import { ok, unauthorized } from "@/application/helpers";
+import { ok, serverError, unauthorized } from "@/application/helpers";
 import { IMiddleware, IRequest, IResponse } from "@/application/protocols";
 import { IDecrypter } from "@/domain/protocols";
 
@@ -7,24 +7,28 @@ export class AuthMiddleware implements IMiddleware {
   constructor(private readonly decrypter: IDecrypter) {}
 
   async handle(request: IRequest): Promise<IResponse> {
-    const { authorization } = request.headers;
+    try {
+      const { authorization } = request.headers;
 
-    if (!authorization) {
-      return unauthorized(new UnauthorizedError());
+      if (!authorization) {
+        return unauthorized(new UnauthorizedError());
+      }
+
+      const [, token] = authorization.split(" ");
+
+      const payload = await this.decrypter.decrypt(token);
+
+      if (!payload) {
+        return unauthorized(new UnauthorizedError());
+      }
+
+      const data = {
+        userId: payload.sub,
+      };
+
+      return ok(data);
+    } catch (error) {
+      return serverError(error as Error);
     }
-
-    const [, token] = authorization.split(" ");
-
-    const payload = await this.decrypter.decrypt(token);
-
-    if (!payload) {
-      return unauthorized(new UnauthorizedError());
-    }
-
-    const data = {
-      userId: payload.sub,
-    };
-
-    return ok(data);
   }
 }
