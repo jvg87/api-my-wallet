143fbb610cbacd14c49d6b357e6f01126f7f8409
João Victor <jvg87.dev@gmail.com>
2025-01-11 20:50:33 -0300

    test: ensure GetUser UseCase calls UserRepository.getById with correct id

---
+++ tests/domain/usecases/get-user-usecase.spec.ts
@@ -0,0 +1,33 @@
+import { GetUserUseCase } from "@/domain/usecases";
+import {
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  jest,
+} from "@jest/globals";
+
+import {
+  mockDetailUser,
+  mockUser,
+  mockUserRepository,
+} from "@/tests/domain/mocks";
+
+describe("GetUser UseCase", () => {
+  let sut: GetUserUseCase;
+
+  beforeAll(() => {
+    mockUserRepository.getById.mockResolvedValue(mockDetailUser());
+  });
+
+  beforeEach(() => {
+    sut = new GetUserUseCase(mockUserRepository);
+  });
+
+  it("Should call UserRepository.getById with correct id", async () => {
+    const getByIdSpy = jest.spyOn(mockUserRepository, "getById");
+    await sut.execute(mockUser().id);
+    expect(getByIdSpy).toHaveBeenCalledWith(mockUser().id);
+  });
+});
