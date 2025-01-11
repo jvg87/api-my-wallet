b5d282f783340711516673b6c73d2f37ff3e8296
João Victor <jvg87.dev@gmail.com>
2025-01-11 17:27:33 -0300

    test: ensure AuthMiddleware calls decrypter with correct token

--- tests/application/middlewares/auth-middleware.spec.ts
+++ tests/application/middlewares/auth-middleware.spec.ts
@@ -1,9 +1,17 @@
-import { beforeEach, describe, expect, it } from "@jest/globals";
+import {
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  jest,
+} from "@jest/globals";
 
 import { UnauthorizedError } from "@/application/erros";
 import { unauthorized } from "@/application/helpers";
 import { AuthMiddleware } from "@/application/middlewares";
 import { IRequest } from "@/application/protocols";
+import { IDecrypter } from "@/domain/protocols";
 
 describe("Auth Middleware", () => {
   const mockRequest: IRequest = {
@@ -12,10 +20,20 @@ describe("Auth Middleware", () => {
     },
   };
 
+  const mockDecrypter: jest.Mocked<IDecrypter> = {
+    decrypt: jest.fn(),
+  };
+
   let sut: AuthMiddleware;
 
   beforeEach(() => {
-    sut = new AuthMiddleware();
+    sut = new AuthMiddleware(mockDecrypter);
+  });
+
+  beforeAll(() => {
+    mockDecrypter.decrypt.mockResolvedValue({
+      sub: "user_id",
+    });
   });
 
   it("Should return 401 if no authorization is provided", async () => {
@@ -23,5 +41,10 @@ describe("Auth Middleware", () => {
       headers: {},
     });
     expect(response).toEqual(unauthorized(new UnauthorizedError()));
+  });
+
+  it("Should call decrypter with valid token", async () => {
+    await sut.handle(mockRequest);
+    expect(mockDecrypter.decrypt).toHaveBeenCalledWith("valid_token");
   });
 });
