42b2ad2537d64ff004b876e969fc0cd193a2d682
João Victor <jvg87.dev@gmail.com>
2025-01-11 17:27:07 -0300

    feat: ensure AuthMiddleware calls decrypter with correct token

--- src/application/middlewares/auth-middleware.ts
+++ src/application/middlewares/auth-middleware.ts
@@ -1,14 +1,22 @@
 import { UnauthorizedError } from "@/application/erros";
 import { unauthorized } from "@/application/helpers";
 import { IMiddleware, IRequest, IResponse } from "@/application/protocols";
+import { IDecrypter } from "@/domain/protocols";
 
 export class AuthMiddleware implements IMiddleware {
+  constructor(private readonly decrypter: IDecrypter) {}
+
   async handle(request: IRequest): Promise<IResponse> {
     const { authorization } = request.headers;
 
     if (!authorization) {
       return unauthorized(new UnauthorizedError());
     }
+
+    const [, token] = authorization.split(" ");
+
+    await this.decrypter.decrypt(token);
+
     return {
       statusCode: 1,
     };
