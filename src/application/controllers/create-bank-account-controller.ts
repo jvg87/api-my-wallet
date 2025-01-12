4359598e64ed39b16626c1457d38c79df99901f7
João Victor <jvg87.dev@gmail.com>
2025-01-12 18:59:54 -0300

    feat: ensure CreateBankAccount Controller returns 400 if no credentials are provided

--- src/application/controllers/create-bank-account-controller.ts
+++ src/application/controllers/create-bank-account-controller.ts
@@ -1,21 +1,47 @@
-import { UnauthorizedError } from "@/application/erros";
-import { unauthorized } from "@/application/helpers";
+import { MissingParamsError, UnauthorizedError } from "@/application/erros";
+import { badRequest, unauthorized } from "@/application/helpers";
 import {
   IController,
   IHttpRequest,
   IHttpResponse,
 } from "@/application/protocols";
+import { BankAccountType } from "@/domain/entities";
 import { ICreateBankAccount } from "@/domain/protocols";
 
 export class CreateBankAccountController implements IController {
   constructor(private readonly createBankAccount: ICreateBankAccount) {}
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
-    const { name, initialBalance, color, type } = request.body;
     const userId = request.userId;
 
     if (!userId) return unauthorized(new UnauthorizedError());
+
+    const { name, initialBalance, color, type } = request.body;
+
+    const missingParam = this.validateParams({
+      name,
+      initialBalance,
+      color,
+      type,
+    });
+
+    if (missingParam) return badRequest(new MissingParamsError(missingParam));
+
     return {
       statusCode: 1,
     };
+  }
+
+  private validateParams(params: {
+    name?: string;
+    initialBalance?: number;
+    color?: string;
+    type?: BankAccountType;
+  }): string | null {
+    if (!params.name) return "name";
+    if (!params.initialBalance) return "initialBalance";
+    if (!params.color) return "color";
+    if (!params.type) return "type";
+
+    return null;
   }
 }
