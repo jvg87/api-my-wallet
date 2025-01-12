bb0a00940f2fe53dc712a7ebb5ac5e23f9bdf857
João Victor <jvg87.dev@gmail.com>
2025-01-11 21:17:21 -0300

    feat: ensure GetUser Controller calls GetUser method with correct id

--- src/application/controllers/get-user-controller.ts
+++ src/application/controllers/get-user-controller.ts
@@ -1,12 +1,20 @@
-import { UnauthorizedError } from "../erros";
-import { unauthorized } from "../helpers";
-import { IController, IHttpRequest, IHttpResponse } from "../protocols";
+import { UnauthorizedError } from "@/application/erros";
+import { unauthorized } from "@/application/helpers";
+import {
+  IController,
+  IHttpRequest,
+  IHttpResponse,
+} from "@/application/protocols";
+import { IGetUser } from "@/domain/protocols";
 
 export class GetUserController implements IController {
+  constructor(private readonly getUser: IGetUser) {}
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
     const { userId } = request;
 
     if (!userId) return unauthorized(new UnauthorizedError());
+
+    await this.getUser.execute(userId);
 
     return {
       statusCode: 1,
