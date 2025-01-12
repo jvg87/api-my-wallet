d6abc3e608718d9bca2a281b695f4b3311ad575c
João Victor <jvg87.dev@gmail.com>
2025-01-11 21:18:41 -0300

    test: ensure GetUser Controller calls GetUser method with correct id

--- tests/application/controllers/get-user-controller.spec.ts
+++ tests/application/controllers/get-user-controller.spec.ts
@@ -1,14 +1,39 @@
-import { beforeEach, describe, expect, it } from "@jest/globals";
+import {
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  jest,
+} from "@jest/globals";
 
 import { GetUserController } from "@/application/controllers";
 import { UnauthorizedError } from "@/application/erros";
 import { unauthorized } from "@/application/helpers";
+import { IHttpRequest } from "@/application/protocols";
+import { IGetUser } from "@/domain/protocols";
 
 describe("GetUser Controller", () => {
+  const request: IHttpRequest = {
+    body: {},
+    userId: "user_id",
+  };
+
+  const mockGetUser: jest.Mocked<IGetUser> = {
+    execute: jest.fn(),
+  };
+
   let sut: GetUserController;
 
+  beforeAll(() => {
+    mockGetUser.execute.mockResolvedValue({
+      email: "user_email@mail.com",
+      name: "user_name",
+    });
+  });
+
   beforeEach(() => {
-    sut = new GetUserController();
+    sut = new GetUserController(mockGetUser);
   });
 
   it("Should return 401 if no user id is provided", async () => {
@@ -17,5 +42,11 @@ describe("GetUser Controller", () => {
       userId: "",
     });
     expect(httpResponse).toEqual(unauthorized(new UnauthorizedError()));
+  });
+
+  it("Should call GetUser method with correct id", async () => {
+    await sut.handle(request);
+    const spy = jest.spyOn(mockGetUser, "execute");
+    expect(spy).toHaveBeenCalledWith("user_id");
   });
 });
