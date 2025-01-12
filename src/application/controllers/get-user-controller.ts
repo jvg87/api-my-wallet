454f20e77ea0d394549a7f6d303702a5e7081b39
João Victor <jvg87.dev@gmail.com>
2025-01-11 21:25:01 -0300

    feat: ensure GetUser Controller returns 500 if GetUser trhows

--- src/application/controllers/get-user-controller.ts
+++ src/application/controllers/get-user-controller.ts
@@ -1,5 +1,5 @@
-import { UnauthorizedError } from "@/application/erros";
-import { ok, unauthorized } from "@/application/helpers";
+import { ServerError, UnauthorizedError } from "@/application/erros";
+import { ok, serverError, unauthorized } from "@/application/helpers";
 import {
   IController,
   IHttpRequest,
@@ -10,12 +10,16 @@ import { IGetUser } from "@/domain/protocols";
 export class GetUserController implements IController {
   constructor(private readonly getUser: IGetUser) {}
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
-    const { userId } = request;
+    try {
+      const { userId } = request;
 
-    if (!userId) return unauthorized(new UnauthorizedError());
+      if (!userId) return unauthorized(new UnauthorizedError());
 
-    const user = await this.getUser.execute(userId);
+      const user = await this.getUser.execute(userId);
 
-    return ok(user);
+      return ok(user);
+    } catch (error) {
+      return serverError(error as ServerError);
+    }
   }
 }
