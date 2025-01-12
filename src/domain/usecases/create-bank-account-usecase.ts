39d1b3fb0c3c3011b18b06100768c3653d6cf32c
João Victor <jvg87.dev@gmail.com>
2025-01-12 18:32:41 -0300

    feat: ensure CreateBankAccount UseCase calls BankAccountRepository.crete with correct values

--- src/domain/usecases/create-bank-account-usecase.ts
+++ src/domain/usecases/create-bank-account-usecase.ts
@@ -3,15 +3,24 @@ import {
   BankAccountParams,
   BankAccountType,
 } from "@/domain/entities";
-import { ICreateBankAccount } from "@/domain/protocols";
+import { IBankAccountRepository, ICreateBankAccount } from "@/domain/protocols";
 
 export class CreateBankAccountUseCase implements ICreateBankAccount {
+  constructor(private readonly bankAccountRepository: IBankAccountRepository) {}
   async execute(
     bankAccountParams: BankAccountParams
   ): Promise<BankAccount | null> {
     const { userId, color, initialBalance, name, type } = bankAccountParams;
 
     if (!userId) return null;
+
+    await this.bankAccountRepository.create({
+      userId,
+      color,
+      initialBalance,
+      name,
+      type,
+    });
 
     return {
       id: "",
