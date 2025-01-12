962c61c483165806a8b1fa6491b39e415251eeb0
João Victor <jvg87.dev@gmail.com>
2025-01-12 20:25:33 -0300

    test: ensure GetAllBankAccounts calls BankAccountRepository.findAllByUserId with correct userId

--- tests/domain/usecases/get-all-bank-accounts-usecase.spec.ts
+++ tests/domain/usecases/get-all-bank-accounts-usecase.spec.ts
@@ -1,8 +1,41 @@
-import { describe, it } from "@jest/globals";
+import { beforeAll, beforeEach, describe, expect, it } from "@jest/globals";
 
+import { BankAccountType } from "@/domain/entities";
 import { GetAllBankAccountsUseCase } from "@/domain/usecases";
+import { mockBankAccountRepository } from "@/tests/domain/mocks";
 
 describe("GetAllBankAccounts UseCase", () => {
+  const userId = "any_user_id";
+
   let sut: GetAllBankAccountsUseCase;
-  it("Should ", async () => {});
+
+  beforeAll(() => {
+    mockBankAccountRepository.findAllByUserId.mockResolvedValue([
+      {
+        color: "any_color",
+        id: "any_id",
+        name: "any_name",
+        initialBalance: 1000,
+        type: BankAccountType.CASH,
+      },
+      {
+        color: "other_color",
+        id: "other_id",
+        name: "other_name",
+        initialBalance: 100,
+        type: BankAccountType.CHECKING,
+      },
+    ]);
+  });
+
+  beforeEach(() => {
+    sut = new GetAllBankAccountsUseCase(mockBankAccountRepository);
+  });
+
+  it("Should call BankAccountRepository.findAllByUserId with correct userId", async () => {
+    await sut.execute(userId);
+    expect(mockBankAccountRepository.findAllByUserId).toHaveBeenCalledWith(
+      userId
+    );
+  });
 });
