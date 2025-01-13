4efe1549c7f69b2f8dfa9bd856e20450dc8ae83d
João Victor <jvg87.dev@gmail.com>
2025-01-13 12:10:05 -0300

    feat: ensure GetAllBankAccountsController calls GetAllBankAccounts with correct id

--- src/application/controllers/get-all-bank-accounts-controller.ts
+++ src/application/controllers/get-all-bank-accounts-controller.ts
@@ -5,12 +5,16 @@ import {
   IHttpRequest,
   IHttpResponse,
 } from "@/application/protocols";
+import { IGetAllBankAccounts } from "@/domain/protocols";
 
 export class GetAllBankAccountsController implements IController {
+  constructor(private readonly getAllBankAccounts: IGetAllBankAccounts) {}
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
     const { userId } = request;
 
     if (!userId) return unauthorized(new UnauthorizedError());
+
+    await this.getAllBankAccounts.execute(userId);
     return {
       statusCode: 1,
     };
