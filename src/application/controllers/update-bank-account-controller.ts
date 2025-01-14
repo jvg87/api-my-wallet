d95e15053d5d0ad5c2f78e39d568df78bc49c6f3
João Victor <jvg87.dev@gmail.com>
2025-01-13 21:35:29 -0300

    feat: ensure UpdateBankAccountController returns 400 if no params are provided

--- src/application/controllers/update-bank-account-controller.ts
+++ src/application/controllers/update-bank-account-controller.ts
@@ -1,10 +1,11 @@
-import { UnauthorizedError } from "@/application/erros";
-import { unauthorized } from "@/application/helpers";
+import { MissingParamsError, UnauthorizedError } from "@/application/erros";
+import { badRequest, unauthorized } from "@/application/helpers";
 import {
   IController,
   IHttpRequest,
   IHttpResponse,
 } from "@/application/protocols";
+import { BankAccountType } from "@/domain/entities";
 
 export class UpdateBankAccountController implements IController {
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
@@ -12,8 +13,33 @@ export class UpdateBankAccountController implements IController {
 
     if (!userId) return unauthorized(new UnauthorizedError());
 
+    const { name, initialBalance, color, type } = request.body;
+
+    const missingParam = this.validateParams({
+      name,
+      initialBalance,
+      color,
+      type,
+    });
+
+    if (missingParam) return badRequest(new MissingParamsError(missingParam));
+
     return {
       statusCode: 1,
     };
+  }
+
+  private validateParams(params: {
+    name?: string;
+    initialBalance?: number;
+    color?: string;
+    type?: BankAccountType;
+  }): string | null {
+    if (!params.name) return "name";
+    if (!params.initialBalance) return "initialBalance";
+    if (!params.color) return "color";
+    if (!params.type) return "type";
+
+    return null;
   }
 }
