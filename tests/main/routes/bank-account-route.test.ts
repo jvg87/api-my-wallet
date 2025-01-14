ec1d1d5640e04dc36cb1cde05634e6a45178dea3
João Victor <jvg87.dev@gmail.com>
2025-01-14 10:43:06 -0300

    test: add UpdateBankAccount route

--- tests/main/routes/bank-account-route.test.ts
+++ tests/main/routes/bank-account-route.test.ts
@@ -1,10 +1,10 @@
 import { afterAll, beforeEach, describe, expect, it } from "@jest/globals";
+import { hash } from "bcrypt";
 import request from "supertest";
 
 import { UnauthorizedError } from "@/application/erros";
 import app from "@/main/config/app";
 import { prisma } from "@/utils";
-import { hash } from "bcrypt";
 
 describe("CreateBankAccount Route", () => {
   afterAll(async () => {
@@ -100,6 +100,68 @@ describe("CreateBankAccount Route", () => {
       expect(body.length).toBe(1);
       expect(body[0].id).toBeDefined();
       expect(body[0].name).toEqual("teste");
+    });
+  });
+
+  describe("PUT /bank-accounts/:bankAccountId", () => {
+    it("Should return 401 if authorization header is not provided", async () => {
+      const { status, body } = await request(app).get("/bank-accounts");
+      expect(status).toBe(401);
+      expect(body).toEqual({ error: new UnauthorizedError().message });
+    });
+
+    it("Should return 200 with list of bank accounts", async () => {
+      const passwordHashed = await hash("any_password", 12);
+
+      await prisma.user.create({
+        data: {
+          name: "any_name",
+          email: "any_mail@mail.com",
+          password: passwordHashed,
+        },
+      });
+
+      const response = await request(app).post("/auth").send({
+        email: "any_mail@mail.com",
+        password: "any_password",
+      });
+
+      await request(app)
+        .post("/bank-accounts")
+        .set({
+          authorization: `Bearer ${response.body.token}`,
+        })
+        .send({
+          name: "teste",
+          initialBalance: 1000,
+          color: "#fff",
+          type: "CASH",
+        });
+
+      const list = await request(app)
+        .get("/bank-accounts")
+        .set({
+          authorization: `Bearer ${response.body.token}`,
+        });
+
+      const userId = list.body[0].userId;
+      const bankAccountId = list.body[0].id;
+
+      const { status, body } = await request(app)
+        .put(`/bank-accounts/${bankAccountId}`)
+        .set({
+          authorization: `Bearer ${response.body.token}`,
+        })
+        .send({
+          userId,
+          name: "update",
+          initialBalance: 1000,
+          color: "#000",
+          type: "CASH",
+        });
+
+      expect(status).toBe(200);
+      expect(body.name).toEqual("update");
     });
   });
 });
