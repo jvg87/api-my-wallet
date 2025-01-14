5ebcac8cb50eff26c0e77db3398ed4aa0798f819
João Victor <jvg87.dev@gmail.com>
2025-01-14 13:08:16 -0300

    feat: ensure DeleteBankAccountUseCase calls UserRepository.findById with correct id

--- src/domain/usecases/bank-account/delete-bank-account-usecase.ts
+++ src/domain/usecases/bank-account/delete-bank-account-usecase.ts
@@ -1,7 +1,10 @@
-import { IDeleteBankAccount } from "@/domain/protocols";
+import { IBankAccountRepository, IDeleteBankAccount } from "@/domain/protocols";
 
 export class DeleteBankAccountUseCase implements IDeleteBankAccount {
+  constructor(private readonly bankAccountRepository: IBankAccountRepository) {}
   async execute(bankAccountId: string, userId: string): Promise<void> {
+    const bankAccount =
+      await this.bankAccountRepository.findById(bankAccountId);
     return;
   }
 }
