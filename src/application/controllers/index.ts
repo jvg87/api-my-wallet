129e4d798f4c6d05c1308cb3640ece6432c50e03
João Victor <jvg87.dev@gmail.com>
2025-01-13 21:32:34 -0300

    feat: ensure UpdateBankAccountController returns 401 if no user id is provided

--- src/application/controllers/index.ts
+++ src/application/controllers/index.ts
@@ -3,3 +3,4 @@ export * from "./create-bank-account-controller";
 export * from "./create-user-controller";
 export * from "./get-all-bank-accounts-controller";
 export * from "./get-user-controller";
+export * from "./update-bank-account-controller";
---
+++ src/application/controllers/update-bank-account-controller.ts
@@ -0,0 +1,19 @@
+import { UnauthorizedError } from "@/application/erros";
+import { unauthorized } from "@/application/helpers";
+import {
+  IController,
+  IHttpRequest,
+  IHttpResponse,
+} from "@/application/protocols";
+
+export class UpdateBankAccountController implements IController {
+  async handle(request: IHttpRequest): Promise<IHttpResponse> {
+    const userId = request.userId;
+
+    if (!userId) return unauthorized(new UnauthorizedError());
+
+    return {
+      statusCode: 1,
+    };
+  }
+}
