e366e33513b3cc6a9202bc8ee7df2933fd8ecdab
João Victor <jvg87.dev@gmail.com>
2025-01-14 13:39:35 -0300

    feat: ensure DeleteBankAccountController returns 500 if DeleteBankAccount throws

--- src/application/controllers/bank-account/delete-bank-account-controller.ts
+++ src/application/controllers/bank-account/delete-bank-account-controller.ts
@@ -1,9 +1,15 @@
 import {
   MissingParamsError,
   NotFoundError,
+  ServerError,
   UnauthorizedError,
 } from "@/application/erros";
-import { badRequest, notFound, unauthorized } from "@/application/helpers";
+import {
+  badRequest,
+  notFound,
+  serverError,
+  unauthorized,
+} from "@/application/helpers";
 import {
   IController,
   IHttpRequest,
@@ -14,22 +20,26 @@ import { IDeleteBankAccount } from "@/domain/protocols";
 export class DeleteBankAccountController implements IController {
   constructor(private readonly deleteBankAccount: IDeleteBankAccount) {}
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
-    const userId = request.userId;
+    try {
+      const userId = request.userId;
 
-    if (!userId) return unauthorized(new UnauthorizedError());
+      if (!userId) return unauthorized(new UnauthorizedError());
 
-    const bankAccountId = request.params?.bankAccountId;
+      const bankAccountId = request.params?.bankAccountId;
 
-    if (!bankAccountId)
-      return badRequest(new MissingParamsError("bankAccountId"));
+      if (!bankAccountId)
+        return badRequest(new MissingParamsError("bankAccountId"));
 
-    const response = await this.deleteBankAccount.execute(
-      bankAccountId,
-      userId
-    );
+      const response = await this.deleteBankAccount.execute(
+        bankAccountId,
+        userId
+      );
 
-    if (!response) return notFound(new NotFoundError());
+      if (!response) return notFound(new NotFoundError());
 
-    return { statusCode: 1 };
+      return { statusCode: 1 };
+    } catch (error) {
+      return serverError(error as ServerError);
+    }
   }
 }
